917a5f490333f1fa95e2856b62da59e9d3bff5cc
Diego Oliveira <diegoliveiraa1@gmail.com>
2024-12-01 13:01:21 -0300

    feat: creat TransactionRepository

---
+++ picpaysimplicado/src/main/java/com/picpaysimplicado/repositories/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.picpaysimplicado.repositories;
+
+import com.picpaysimplicado.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
