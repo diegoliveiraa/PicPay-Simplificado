13419a41b147137208e5f91d313757ce17bbc5ab
Diego Oliveira <diegoliveiraa1@gmail.com>
2024-12-01 22:19:01 -0300

    feat: fix parameter error

--- picpaysimplicado/src/main/java/com/picpaysimplicado/dtos/TransactionDTO.java
+++ picpaysimplicado/src/main/java/com/picpaysimplicado/dtos/TransactionDTO.java
@@ -2,5 +2,5 @@ package com.picpaysimplicado.dtos;
 
 import java.math.BigDecimal;
 
-public record TransactionDTO(BigDecimal value, Long sendId, Long receiverId) {
+public record TransactionDTO(BigDecimal value, Long senderId, Long receiverId) {
 }
